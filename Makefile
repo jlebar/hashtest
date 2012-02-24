f5dae82969ede3e30bf4a0c8d46cf5571dff5865
Justin Lebar <justin.lebar@gmail.com>
2012-02-24 11:43:01 +0100

    Updating test to include nscrt's hash.

--- Makefile
+++ Makefile
@@ -15,12 +15,10 @@ spookyhash:
 testatoms:
 	g++ -O3 -Wall -Werror -c -Icityhash/src -Ismhasher -Ispookyhash testatoms.cpp
 
-link:
+link: cityhash smhasher spookyhash testatoms
 	g++ -O3 -Wall -Werror -o testatoms testatoms.o spookyhash/spooky.o cityhash/src/city.o smhasher/libSmhasherSupport.a
 
 .PHONY: all compile test cityhash smhasher spookyhash testatoms
 
 test: compile
-	./testatoms spookyhash atoms | sort | uniq -c | grep -v '^ *1 ' || echo "No collisions"
-	./testatoms murmurhash atoms | sort | uniq -c | grep -v '^ *1 ' || echo "No collisions"
-	./testatoms cityhash atoms | sort | uniq -c | grep -v '^ *1 ' || echo "No collisions"
+	./testatoms atoms
--- testatoms.cpp
+++ testatoms.cpp
@@ -2,6 +2,7 @@
 #include "Murmurhash3.h"
 #include "city.h"
 #include <vector>
+#include <map>
 #include <fstream>
 #include <string>
 #include <sys/time.h>
@@ -24,7 +25,7 @@ public:
     double elapsed = endTime.tv_sec  - startTime.tv_sec +
                      (endTime.tv_usec - startTime.tv_usec) / 1e6;
 
-    fprintf(stderr, "%0.2fms elapsed\n", elapsed * 1000);
+    printf("%0.2fms elapsed\n", elapsed * 1000);
   }
 
 private:
@@ -34,8 +35,28 @@ private:
 enum hash_name {
   HASH_SPOOKYHASH,
   HASH_CITYHASH,
-  HASH_MURMURHASH
+  HASH_MURMURHASH,
+  HASH_NSCRT
 };
+
+#define PR_ROTATE_LEFT32(a, bits) (((a) << (bits)) | ((a) >> (32 - (bits))))
+#define ADD_TO_HASHVAL(hashval, c) \
+    hashval = PR_ROTATE_LEFT32(hashval, 4) ^ (c);
+
+uint32_t NSCrtHash(const char* start, size_t length)
+{
+  uint32_t h = 0;
+  const char* s = start;
+  const char* end = start + length;
+
+  unsigned char c;
+  while ( s < end ) {
+    c = *s++;
+    ADD_TO_HASHVAL(h, c);
+  }
+
+  return h;
+}
 
 template<hash_name hashfn>
 inline uint32_t hash(const string& line)
@@ -52,13 +73,67 @@ inline uint32_t hash(const string& line)
     MurmurHash3_x64_128(line.c_str(), line.length(), 0, &result);
     return result[0];
   }
+  else if (hashfn == HASH_NSCRT) {
+    return NSCrtHash(line.c_str(), line.length());
+  }
 
   return -1;
 }
 
 template<hash_name hashfn>
-void test(const char* filename)
+void test(const vector<string>& lines, const char* testname)
 {
+  printf("Testing %s\n", testname);
+
+  {
+    AutoTimer timer;
+    for (size_t i = 0; i < 1000; i++) {
+      for (size_t j = 0; j < lines.size(); j++) {
+        hash<hashfn>(lines[i]);
+      }
+    }
+  }
+
+  map<uint32_t, uint32_t> hashes;
+  for (size_t i = 0; i < lines.size(); i++) {
+    uint32_t h = hash<hashfn>(lines[i]);
+    hashes[h]++;
+  }
+
+  bool foundCollision = false;
+  map<uint32_t, uint32_t> collisions;
+  for (map<uint32_t, uint32_t>::iterator i = hashes.begin(); i != hashes.end(); ++i) {
+    collisions[i->second]++;
+    if (i->second > 1) {
+      foundCollision = true;
+    }
+  }
+
+  if (!foundCollision) {
+    printf("No collisions\n");
+  }
+  else {
+    for (map<uint32_t, uint32_t>::iterator i = collisions.begin(); i != collisions.end(); ++i) {
+      if (i->first == 1) {
+        printf("        unique: %6d\n", i->second);
+      }
+      else {
+        printf("%3d collisions: %6d\n", i->first, i->second);
+      }
+    }
+  }
+
+  printf("\n");
+}
+
+int main(int argc, const char **argv)
+{
+  if (argc != 2) {
+    printf("Usage: %s FILE\n", argv[0]);
+    return 1;
+  }
+
+  const char* filename = argv[1];
   ifstream fin(filename);
   vector<string> lines;
 
@@ -71,40 +146,10 @@ void test(const char* filename)
     }
   }
 
-  {
-    AutoTimer timer;
-    for (size_t i = 0; i < 1000; i++) {
-      for (size_t j = 0; j < lines.size(); j++) {
-        hash<hashfn>(lines[i]);
-      }
-    }
-  }
-
-  for (size_t i = 0; i < lines.size(); i++) {
-    printf("'%s': %08x\n", lines[i].c_str(), hash<hashfn>(lines[i]));
-  }
-}
-
-int main(int argc, const char **argv)
-{
-  if (argc != 3) {
-    printf("Usage: %s (spookyhash|murmurhash|cityhash) FILE\n", argv[0]);
-    return 1;
-  }
-
-  if (strcmp(argv[1], "spookyhash") == 0) {
-    test<HASH_SPOOKYHASH>(argv[2]);
-  }
-  else if (strcmp(argv[1], "cityhash") == 0) {
-    test<HASH_CITYHASH>(argv[2]);
-  }
-  else if (strcmp(argv[1], "murmurhash") == 0) {
-    test<HASH_MURMURHASH>(argv[2]);
-  }
-  else {
-    printf("Unknown hash name.\n");
-    return 1;
-  }
+  test<HASH_SPOOKYHASH>(lines, "spookyhash");
+  test<HASH_CITYHASH>(lines, "cityhash");
+  test<HASH_MURMURHASH>(lines, "murmurhash");
+  test<HASH_NSCRT>(lines, "nscrt");
 
   return 0;
 }
